f4874315857889f68ed6f2b4042714c8c19aa0e2
Camila da Silva <casilvapere@gmail.com>
2025-06-16 19:58:07 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="mater

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import HeroSection from '@/components/sections/HeroSection';
 import AboutSection from '@/components/sections/AboutSection';
 import KitDescriptionSection from '@/components/sections/KitDescriptionSection';
 import HowToReceiveSection from '@/components/sections/HowToReceiveSection';
-import ActivityShowcaseSection from '@/components/sections/ActivityShowcaseSection';
 import BenefitsSection from '@/components/sections/BenefitsSection';
 import TestimonialsSection from '@/components/sections/TestimonialsSection';
 import CtaSection from '@/components/sections/CtaSection';
@@ -19,7 +18,7 @@ export default function HomePage() {
         <AboutSection />
         <HowToReceiveSection />
         <KitDescriptionSection />
-        <ActivityShowcaseSection />
+        {/* ActivityShowcaseSection was here */}
         <BenefitsSection />
         <TestimonialsSection />
         <CtaSection />
