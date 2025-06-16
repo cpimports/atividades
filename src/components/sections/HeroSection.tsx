4e09a604dc9f13cffd3c344a31158780e11fbcc6
Camila da Silva <casilvapere@gmail.com>
2025-06-16 17:46:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
         {/* Subtle background pattern or image can be added here */}
       </div>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center relative z-10">
-        <div className="mb-6 mt-[-2rem] md:mt-[-3rem] animate-fade-in-fast flex justify-center">
+        <div className="mb-6 mt-[-3rem] md:mt-[-4rem] animate-fade-in-fast flex justify-center">
           <Image
             src="https://i.imgur.com/sb4rEa0.png"
             alt="Spectrum Sprouts Logo"
