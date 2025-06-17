c6d3da2eac78e7bbef1ab90c3d8c925017798e8e
Camila da Silva <casilvapere@gmail.com>
2025-06-17 23:49:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/CtaSection.tsx
+++ src/components/sections/CtaSection.tsx
@@ -23,8 +23,8 @@ export default function CtaSection() {
                 src="https://i.imgur.com/lbSS21Q.png"
                 alt="Garantia de satisfação"
                 data-ai-hint="shield checkmark"
-                width={96}
-                height={96}
+                width={128}
+                height={128}
                 className="mb-3"
               />
               <CardTitle className="font-headline text-2xl text-primary">SUA SATISFAÇÃO GARANTIDA OU SEU DINHEIRO DE VOLTA!</CardTitle>
