455cf65475e045ff45ad424d4050d93072dd662c
Camila da Silva <casilvapere@gmail.com>
2025-06-17 00:08:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/KitDescriptionSection.tsx
+++ src/components/sections/KitDescriptionSection.tsx
@@ -10,8 +10,8 @@ export default function KitDescriptionSection() {
             src="https://i.imgur.com/OnWpWgf.png"
             alt="Ícone de livro com atividades"
             data-ai-hint="book activities icon"
-            width={64}
-            height={64}
+            width={96}
+            height={96}
             className="mx-auto mb-4 rounded-md"
           />
           <h2 className="font-headline text-3xl sm:text-4xl font-semibold text-white">
@@ -30,3 +30,4 @@ export default function KitDescriptionSection() {
     </section>
   );
 }
+
