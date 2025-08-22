764c470954f960381e2a9c934e86f94b0384eead
Camila da Silva <casilvapere@gmail.com>
2025-08-22 21:04:23 +0000

    Da última vez que te pedi para colocar o código do pixel na head da pági

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
       <head>
         {/* Preconnect to critical third-party origins */}
         <link rel="preconnect" href="https://i.imgur.com" />
-        <link rel="preconnect" href="https://connect.facebook.net" />
         <link rel="preconnect" href="https://scripts.converteai.net" />
         <link rel="preconnect" href="https://cdn.converteai.net" />
         <link rel="preconnect" href="https://images.converteai.net" />
@@ -63,22 +62,6 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://images.converteai.net" />
         <link rel="dns-prefetch" href="https://api.vturb.com.br" />
 
-        {/* Meta Pixel Code */}
-        <Script id="facebook-pixel-base" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src='https://connect.facebook.net/en_US/fbevents.js';
-            s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script');
-            fbq('init', '1650519618943117');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-
         {/* UTMify Pixel Loader */}
         <Script id="utmify-pixel-loader" strategy="afterInteractive">
           {`
@@ -92,25 +75,17 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="font-body antialiased">
-        <noscript>
-          <img height="1" width="1" style={{display: 'none'}}
-          src="https://www.facebook.com/tr?id=1650519618943117&ev=PageView&noscript=1"/>
-        </noscript>
         
         <AnnouncementBar />
         {children}
         <Toaster />
         <SocialProofNotification />
         
-        {/* InitiateCheckout Event Script for FB and UTMify */}
+        {/* InitiateCheckout Event Script for UTMify */}
         <Script id="initiate-checkout-tracker" strategy="lazyOnload">
           {`
             document.addEventListener('click', function(e) {
               if (e.target && e.target.closest('#btn-comprar')) {
-                // Facebook Pixel
-                if (typeof fbq === 'function') {
-                  fbq('track', 'InitiateCheckout');
-                }
                 // UTMify Pixel
                 if (window.utmify_pixel && typeof window.utmify_pixel.track === 'function') {
                   window.utmify_pixel.track('InitiateCheckout');
