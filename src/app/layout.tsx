8e63a507f645d6a001fd42351bfd47ee9e1a9882
João Carlos <joaocarlosalberti2001@gmail.com>
2025-09-10 18:17:28 +0000

    ao passar o mouse por cima do botão da seção de compra, o botão "COMPRAR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,14 +67,6 @@ export default function RootLayout({
           document.head.appendChild(a);
         `}
         </Script>
-        {/* UTMify Params Loader */}
-        <script
-          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          data-utmify-prevent-xcod-sck
-          data-utmify-prevent-subids
-          async
-          defer
-        ></script>
       </head>
       <body className="font-body antialiased">
         
